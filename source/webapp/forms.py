c2f4ab7d1c9c9da2fd6ddc5edea08a51e43fd1ea
Aziret Arlen <azerarlen312@gmail.com>
2020-08-01 14:43:05 +0600

    Прозрачный текст

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,8 +5,10 @@ default_status = STATUS_CHOICES[0][0]
 
 
 class BookForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Имя', initial='Name')
-    author = forms.CharField(max_length=40, required=True, label='Email', initial='Email Address')
+    title = forms.CharField(max_length=200, required=True, label='Имя',
+                            widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    author = forms.CharField(max_length=40, required=True, label='Email',
+                             widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     text = forms.CharField(max_length=3000, required=True, label="Текст",
                            widget=forms.Textarea)
     status = forms.ChoiceField(choices=STATUS_CHOICES, required=True, label='Статус',
