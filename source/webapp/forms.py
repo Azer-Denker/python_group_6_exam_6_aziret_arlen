a4705098d12741ad9bd92dedba9289f9ddcffb2b
Aziret Arlen <azerarlen312@gmail.com>
2020-08-01 15:20:51 +0600

    Прозрачный текст

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -8,7 +8,7 @@ class BookForm(forms.Form):
     title = forms.CharField(max_length=200, required=True, label='Имя',
                             widget=forms.TextInput(attrs={'placeholder': 'Name'}))
     author = forms.CharField(max_length=40, required=True, label='Email',
-                             widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+                             widget=forms.TextInput(attrs={'placeholder': 'Email Address'}))
     text = forms.CharField(max_length=3000, required=True, label="Текст",
                            widget=forms.Textarea)
     status = forms.ChoiceField(choices=STATUS_CHOICES, required=True, label='Статус',
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,8 +9,8 @@ STATUS_CHOICES = [
 
 class Book(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, default='Name', verbose_name='Имя')
-    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
     author = models.EmailField(max_length=40, null=False, blank=False, default='Email Address', verbose_name='Емейл')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Модерация')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
@@ -19,5 +19,5 @@ class Book(models.Model):
         return '{}. {}'.format(self.pk, self.title)
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
